db05efe0e369e4891dac2fde1038dae02c24df98
XueLing-c <56784633+XueLing-c@users.noreply.github.com>
2019-12-15 14:39:05 +0800

    Create CheckingAccount.h
    This is my assignment_04
    Student ID:201816040114

---
+++ 201816040114/CheckingAccount.h
@@ -0,0 +1,27 @@
+// Lab 1: CheckingAccount.h
+// Definition of CheckingAccount class.
+#ifndef CHECKING_H
+#define CHECKING_H
+
+#include "Account.h" // Account class definition
+
+class CheckingAccount : public Account
+{
+public:
+   // constructor initializes balance and transaction fee
+   CheckingAccount( double, double );
+
+   /* Write a function prototype for virtual function credit,
+      which will redefine the inherited credit function */
+      virtual void credit(double);
+   /* Write a function prototype for virtual function debit,
+      which will redefine the inherited debit function */
+      virtual bool debit(double);
+private:
+   double transactionFee; // fee charged per transaction
+
+   // utility function to charge fee
+   void chargeFee();
+}; // end class CheckingAccount
+
+#endif
