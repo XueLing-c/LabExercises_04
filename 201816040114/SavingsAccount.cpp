aa18b6b671b3318bd3ec97f5b9b7247f6c2b6ad2
XueLing-c <56784633+XueLing-c@users.noreply.github.com>
2019-12-15 14:41:52 +0800

    Create SavingsAccount.cpp
    This is my lab_04
    Student ID:201816040114

---
+++ 201816040114/SavingsAccount.cpp
@@ -0,0 +1,16 @@
+// Lab 1: SavingsAccount.cpp
+// Member-function definitions for class SavingsAccount.
+#include "SavingsAccount.h" // SavingsAccount class definition
+
+// constructor initializes balance and interest rate
+SavingsAccount::SavingsAccount( double initialBalance, double rate )
+   : Account( initialBalance ) // initialize base class
+{
+   interestRate = ( rate < 0.0 ) ? 0.0 : rate; // set interestRate
+} // end SavingsAccount constructor
+
+// return the amount of interest earned
+double SavingsAccount::calculateInterest()
+{
+   return getBalance() * interestRate;
+} // end function calculateInterest
